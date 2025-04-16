3b05bc5a960476a706b8cc27ea476dac656ac779
Charles Leng-uch <clen678@aucklanduni.ac.nz>
2025-04-16 16:08:28 +1200

    springboot setup

---
+++ api_spring/src/test/java/dev/clen678techdemo/api_spring/ApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.clen678techdemo.api_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
